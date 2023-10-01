fb0e64a0defdbb0502ef4b8946acafd93330dc89
OMGDuke <alex@alexrobertson.dev>
2023-10-01 18:35:18 +0100

    fixes context menu not loading on steamos 3.5

--- src/lib/patchContextMenu.tsx
+++ src/lib/patchContextMenu.tsx
@@ -5,7 +5,7 @@ import {
   afterPatch,
   fakeRenderComponent,
   findInReactTree,
-  findInTree,
+  findModuleChild,
   MenuItem,
   Navigation
 } from 'decky-frontend-lib'
@@ -24,78 +24,97 @@ function ChangeMusicButton({ appId }: { appId: number }) {
   )
 }
 
-const spliceArtworkItem = (children: any[], appid: number) => {
+const spliceChangeMusic = (children: any[], appid: number) => {
+  children.find((x: any) => x?.key === 'properties')
+  const propertiesMenuItemIdx = children.findIndex((item) =>
+    findInReactTree(
+      item,
+      (x) => x?.onSelected && x.onSelected.toString().includes('AppProperties')
+    )
+  )
   children.splice(
-    -1,
+    propertiesMenuItemIdx,
     0,
     <ChangeMusicButton key="game-theme-music-change-music" appId={appid} />
   )
 }
+const renderedMap: { [appId: string]: true } = {}
 
+/**
+ * Patches the game context menu.
+ * @param LibraryContextMenu The game context menu.
+ * @returns A patch to remove when the plugin dismounts.
+ */
 const contextMenuPatch = (LibraryContextMenu: any) => {
   return afterPatch(
     LibraryContextMenu.prototype,
     'render',
     (_: Record<string, unknown>[], component: any) => {
       const appid: number = component._owner.pendingProps.overview.appid
-      afterPatch(
-        component.type.prototype,
-        'shouldComponentUpdate',
-        ([nextProps]: any, shouldUpdate: any) => {
-          if (
-            shouldUpdate === true &&
-            !nextProps.children.find(
-              (x: any) => x?.key === 'game-theme-music-change-music'
-            )
-          ) {
-            let updatedAppid: number = appid
-            const parentOverview = nextProps.children.find(
-              (x: any) =>
-                x?._owner?.pendingProps?.overview?.appid &&
-                x._owner.pendingProps.overview.appid !== appid
-            )
-            if (parentOverview) {
-              updatedAppid = parentOverview._owner.pendingProps.overview.appid
-            }
-            spliceArtworkItem(nextProps.children, updatedAppid)
-          }
-          return shouldUpdate
-        },
-        { singleShot: true }
-      )
 
-      spliceArtworkItem(component.props.children, appid)
+      if (
+        !Object.keys(renderedMap).includes(appid.toString()) &&
+        !window.location.pathname.endsWith('/routes/library/home')
+      ) {
+        renderedMap[appid.toString()] = true
+
+        afterPatch(
+          component.type.prototype,
+          'shouldComponentUpdate',
+          ([nextProps]: any, shouldUpdate: any) => {
+            const sgdbIdx = nextProps.children.findIndex(
+              (x: any) => x?.key === 'sgdb-change-artwork'
+            )
+            if (sgdbIdx != -1) nextProps.children.splice(sgdbIdx, 1)
+
+            if (shouldUpdate === true) {
+              let updatedAppid: number = appid
+              // find the first menu component that has the correct appid assigned to _owner
+              const parentOverview = nextProps.children.find(
+                (x: any) =>
+                  x?._owner?.pendingProps?.overview?.appid &&
+                  x._owner.pendingProps.overview.appid !== appid
+              )
+              // if found then use that appid
+              if (parentOverview) {
+                updatedAppid = parentOverview._owner.pendingProps.overview.appid
+              }
+              spliceChangeMusic(nextProps.children, updatedAppid)
+            }
+
+            return shouldUpdate
+          },
+          { singleShot: true }
+        )
+      } else {
+        spliceChangeMusic(component.props.children, appid)
+      }
+
       return component
     }
   )
 }
 
-export const getMenu = async () => {
-  while (!(window as any).DeckyPluginLoader?.routerHook?.routes) {
-    await new Promise((resolve) => setTimeout(resolve, 500))
-  }
-
-  let LibraryContextMenu = findInReactTree(
-    fakeRenderComponent(
-      findInTree(
-        fakeRenderComponent(
-          (window as any).DeckyPluginLoader.routerHook.routes.find(
-            (x: any) => x?.props?.path == '/zoo'
-          ).props.children.type
-        ),
-        (x) => x?.route === '/zoo/modals',
-        {
-          walkable: ['props', 'children', 'child', 'pages']
-        }
-      ).content.type
-    ),
-    (x) => x?.title?.includes('AppActionsMenu')
-  ).children.type
-
-  if (!LibraryContextMenu?.prototype?.AddToHidden) {
-    LibraryContextMenu = fakeRenderComponent(LibraryContextMenu).type
-  }
-  return LibraryContextMenu
-}
+/**
+ * Game context menu component.
+ */
+export const LibraryContextMenu = fakeRenderComponent(
+  findModuleChild((m) => {
+    if (typeof m !== 'object') return
+    for (const prop in m) {
+      if (
+        m[prop]?.toString() &&
+        m[prop].toString().includes('().LibraryContextMenu')
+      ) {
+        return Object.values(m).find(
+          (sibling) =>
+            sibling?.toString().includes('createElement') &&
+            sibling?.toString().includes('navigator:')
+        )
+      }
+    }
+    return
+  })
+).type
 
 export default contextMenuPatch
